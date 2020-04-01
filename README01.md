92fb7e4965c783e3668b84e0533e213580e33105
Peter Schwarck <peterschwarck@gmail.com>
2020-04-01 20:53:10 +0000

    Navbar and grid completed

---
+++ README01.md
@@ -0,0 +1,16 @@
+DailyDoseOfDuck
+
+I. Introduction
+This is a website project showcasing duck (and bird) videos a photos. 
+
+The site is built with JavaScript (react) using Flux. Currently, there is no backend portion. 
+fdgdfkopj;oih;in
+
+
+
+
+
+
+
+
+
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,17 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+//import MuscovyDuck from "../../img/MuscovyDuck.jpg";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="homeContainer">
+		<div className="row">
+			<div className="col-12 col-lg-10 col-md-10 mx-auto topRow">text</div>
+		</div>
+		<div className="row">
+			<div className="col-12 col-lg-8 col-md-10 mx-auto middleRow">text</div>
+		</div>
+		<div className="row">
+			<div className="col-12 col-lg-8 col-md-10 mx-auto bottomRow">text</div>
+		</div>
 	</div>
 );
